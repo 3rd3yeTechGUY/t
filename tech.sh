9a9eb72f27fc9c5866b077401583f10afc2cf920
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-02 00:15:11 +0530

    Update tech.sh

--- tech.sh
+++ tech.sh
@@ -9,10 +9,18 @@ echo "
       |#Github   : rixon-cochi                |
       |#YouTube  : TechCochi                  |
       |     SUBSCRIBE OUR YOUTUBE CHANNEL     |
-      |                                       
+      |                                       |
       +=======================================+
       |..........  TECH COCHI  v1.0  .........|
       +---------------------------------------+" |lolcat
-
-
 sleep 1
+print "[                    ] 0% " | lolcat
+time.sleep(5)
+print "[=====               ] 25%" | lolcat
+time.sleep(5)
+print "[==========          ] 50%" | lolcat
+time.sleep(5)
+print "[===============     ] 75%" | lolcat
+time.sleep(5)
+print "[====================] 100%" | lolcat
+time.sleep(3)
