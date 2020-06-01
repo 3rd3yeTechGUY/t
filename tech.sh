e6652d8162f66912bba073e94e868dfeb7eef8f4
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-02 00:38:47 +0530

    Update tech.sh

--- tech.sh
+++ tech.sh
@@ -16,12 +16,44 @@ echo "
 sleep 1
 
 echo "[                    ] 0% " | lolcat
-sleep 5
-echo "[=====               ] 25%" | lolcat
-sleep 5
-echo "[==========          ] 50%" | lolcat
-sleep 5
-echo "[===============     ] 75%" | lolcat
-sleep 5
-echo "[====================] 100%" | lolcat
-sleep 3
+sleep 1
+echo "[=                   ] 5% " | lolcat
+sleep 1
+echo "[==                  ] 10% " | lolcat
+sleep 1
+echo "[===                 ] 15% " | lolcat
+sleep 1
+echo "[====                ] 20% " | lolcat
+sleep 1
+echo "[=====               ] 25% " | lolcat
+sleep 1
+echo "[======              ] 30% " | lolcat
+sleep 1
+echo "[=======             ] 35% " | lolcat
+sleep 1
+echo "[========            ] 40% " | lolcat
+sleep 1
+echo "[=========           ] 45% " | lolcat
+sleep 1
+echo "[==========          ] 50% " | lolcat
+sleep 1
+echo "[===========         ] 55% " | lolcat
+sleep 1
+echo "[============        ] 60% " | lolcat
+sleep 1
+echo "[=============       ] 65% " | lolcat
+sleep 1
+echo "[==============      ] 70% " | lolcat
+sleep 1
+echo "[===============     ] 75% " | lolcat
+sleep 1
+echo "[================    ] 80% " | lolcat
+sleep 1
+echo "[=================   ] 85% " | lolcat
+sleep 1
+echo "[==================  ] 90% " | lolcat
+sleep 1
+echo "[=================== ] 95% " | lolcat
+sleep 1
+echo "[====================] 100% " | lolcat
+sleep 1
