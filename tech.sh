1061c8174bf3404d5338d58f2702a810cd815fa8
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-01 23:18:48 +0530

    Create tech.sh

---
+++ tech.sh
@@ -0,0 +1,18 @@
+figlet HACKER  | lolcat
+
+
+echo  "       +=======================================+" |lolcat
+echo  "       |..........  TECH COCHI  v1.0  .........|" |lolcat
+echo  "       +---------------------------------------+" |lolcat
+echo  "       |#Author   : TECH COCHI                 |" |lolcat
+echo  "       |#Instagram: tech_cochi                 |" |lolcat
+echo  "       |#Github   : rixon-cochi                |" |lolcat
+echo  "       |#YouTube  : TechCochi                  |" |lolcat
+echo  "       |     SUBSCRIBE OUR YOUTUBE CHANNEL     |" |lolcat
+echo  "       |                                       |" |lolcat
+echo  "       +=======================================+" |lolcat
+echo  "       |..........  TECH COCHI  v1.0  .........|" |lolcat
+echo  "       +---------------------------------------+" |lolcat
+
+
+sleep 1
