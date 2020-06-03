1df09b14eb8d5ed1848af3c26c9211cb38595ad8
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-03 13:06:07 +0530

    Create colourcode.sh

---
+++ colourcode.sh
@@ -0,0 +1,15 @@
+#usr/bin/bash
+clear
+b='\033[34;1m' #blue
+gr='\033[32;1m' #green
+m='\033[35;1m' #magenta
+c='\033[36;1m' #cyan
+r='\033[31;1m' #red
+g='\033[37;1m' #gray
+y='\033[33;1m' #yellow
+# Jangan Recode tech cochi
+
+echo
+echo
+echo
+echo $gr"["$r"•"$gr"]"$m"───────────────────────────────────────────"$gr"["$r"•"$gr"]"
