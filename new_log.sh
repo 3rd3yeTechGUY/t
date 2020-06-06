3584d37639240f2f72015526760231d366825e94
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-06 17:10:14 +0530

    Create new_log.sh

---
+++ new_log.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+b='\033[34;1m' #blue
+gr='\033[32;1m' #green
+m='\033[35;1m' #magenta
+c='\033[36;1m' #cyan
+r='\033[31;1m' #red
+g='\033[37;1m' #gray
+y='\033[33;1m' #yellow
+# Code Recode tech cochi
+
+echo
+echo
+echo
+echo
+echo $gr"["$r"•"$gr"]=============================["$r"•"$gr"]"
+echo  $gr"|"    $c"Author" : Tech Cochi      $gr"|"
+echi  |     GitHub : rixon-cochi      |
+echo  |                               |
+echo  |      YOUTUBE TECH COCHI       | 
+echo  |                               |
+echo [•]=============================[•]
