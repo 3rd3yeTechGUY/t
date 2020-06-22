696f8a52d198351856c778ec727282d609648290
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-23 02:54:53 +0530

    Update c.sh

--- c.sh
+++ c.sh
@@ -7,12 +7,12 @@ command -v unzip > /dev/null 2>&1 || { echo >&2 "Unzip is not installed ! Instal
 }
 banner() {
 clear
-printf " \e[33;1m   _____ __  __ _    _  \e[0m\n"
-printf " \e[33;1m  / ____|  \/  | |  | | \e[0m\n"
-printf " \e[33;1m | (___ | \  / | |__| | \e[0m\n"
-printf " \e[33;1m  \___ \| |\/| |  __  | \e[0m\n"
-printf " \e[33;1m  ____) | |  | | |  | | \e[0m\n"
-printf " \e[33;1m |_____/|_|  |_|_|  |_| \e[92m v1.∅ \e[0m\n"
+printf " \e[33;1m   _____ __  __ _    _  \e[0m\e[0m\e[1;92m \e[0m\n"         
+printf " \e[33;1m  / ____|  \/  | |  | | \e[0m\e[0m\e[1;92m        .-.      _______ \e[0m\n"
+printf " \e[33;1m | (___ | \  / | |__| | \e[0m\e[0m\e[1;92m       {}``; |==|_______D \e[0m\n"
+printf " \e[33;1m  \___ \| |\/| |  __  | \e[0m\e[0m\e[1;92m       / ('        /|\ \e[0m\n"
+printf " \e[33;1m  ____) | |  | | |  | | \e[0m\e[0m\e[1;92m   (  /  |        / | \ \e[0m\n"
+printf " \e[33;1m |_____/|_|  |_|_|  |_| \e[0m\e[0m\e[1;92m akg\(_)_]]      /  |  \ \e[0m\n"
 printf " \e[1;31m ⚠️ Code by Tech Cochi ⚠️\e[0m\n"
 printf " \n"
 }
