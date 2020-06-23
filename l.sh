b47bab6effe79ae4927f1bf6e74d32d127dfc25b
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-23 12:22:23 +0530

    Update l.sh

--- l.sh
+++ l.sh
@@ -4,4 +4,5 @@ printf " \e[91m ██╔════╝\e[0m\e[1;92m ████╗ ███�
 printf " \e[91m ███████╗\e[0m\e[1;92m ██╔████╔██║ \e[0m\e[1;93m █████╗ \e[0m\n" 
 printf " \e[91m ╚════██║\e[0m\e[1;92m ██║╚██╔╝██║ \e[0m\e[1;93m ██╔══╝ \e[0m\n" 
 printf " \e[91m ███████║\e[0m\e[1;92m ██║ ╚═╝ ██║ \e[0m\e[1;93m ██║ \e[0m\n"     
-printf " \e[91m ╚══════╝\e[0m\e[1;92m ╚═╝     ╚═╝ \e[0m\e[1;93m ╚═╝ \e[0m\n"    
+printf " \e[91m ╚══════╝\e[0m\e[1;92m ╚═╝     ╚═╝ \e[0m\e[1;93m ╚═╝ \e[0m\n" 
+printf " \e[44m   CODE                  RECORDED BY               TECH COCHI
