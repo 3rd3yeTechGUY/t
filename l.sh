316e85c20a6a3f891df7af3472e30a88533bd0f3
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-06-23 12:30:26 +0530

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,8 +1,8 @@
 
-printf " \e[91m ███████╗\e[0m\e[1;92m ███╗   ███╗ \e[0m\e[1;93m ███████╗ \e[0m\n"
-printf " \e[91m ██╔════╝\e[0m\e[1;92m ████╗ ████║ \e[0m\e[1;93m ██╔════╝ \e[0m\n"
-printf " \e[91m ███████╗\e[0m\e[1;92m ██╔████╔██║ \e[0m\e[1;93m █████╗ \e[0m\n" 
-printf " \e[91m ╚════██║\e[0m\e[1;92m ██║╚██╔╝██║ \e[0m\e[1;93m ██╔══╝ \e[0m\n" 
-printf " \e[91m ███████║\e[0m\e[1;92m ██║ ╚═╝ ██║ \e[0m\e[1;93m ██║ \e[0m\n"     
-printf " \e[91m ╚══════╝\e[0m\e[1;92m ╚═╝     ╚═╝ \e[0m\e[1;93m ╚═╝ \e[0m\n" 
-printf " \e[44m   CODE                  RECORDED BY               TECH COCHI
+printf " \e[91m ███████╗\e[0m\e[1;92m ███╗   ███╗ \e>
+printf " \e[91m ██╔════╝\e[0m\e[1;92m ████╗ ████║ \e>
+printf " \e[91m ███████╗\e[0m\e[1;92m ██╔████╔██║ \e>
+printf " \e[91m ╚════██║\e[0m\e[1;92m ██║╚██╔╝██║ \e>
+printf " \e[91m ███████║\e[0m\e[1;92m ██║ ╚═╝ ██║ \e>
+printf " \e[91m ╚══════╝\e[0m\e[1;92m ╚═╝     ╚═╝ \e>
+printf " \e[44m   CODE RECORED BY TECH COCHI \e[0m\n"
